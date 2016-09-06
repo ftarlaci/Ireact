ab5a6e706cd7824618ef8ae4d68988d719d81d72
ftarlaci <ftarlaci@utexas.edu>
2016-09-06 11:32:15 -0500

    components added

--- app/app.js
+++ app/app.js
@@ -3,10 +3,11 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 // This code renders a simple HTML Div.
-// The key takeaway here is that we render an ENTIRE DIV. 
+// The key takeaway here is that we render an ENTIRE DIV. // /* It is "className" and not "class" in react when we use bootstap */
 // Note that ReactDOM.render cannot render a block of HTML elements witout them all fitting within a single div or container.
 ReactDOM.render(
 
 		<p>Hello World!</p>,
 	document.getElementById('app')
 )
+
